631b108b0ffc95b179e57a8075dcf877a15fd14c
shon <turayevshahzodbek@gmail.com>
2024-04-06 23:31:42 +0900

    feat: practice 3 easy is solved

---
+++ easy_practice/task-2.ts
@@ -0,0 +1,18 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+function twoSum(nums: number[], target: number): number[] {
+  let a: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        a.push(nums[i]);
+      }
+    }
+  }
+  let last = a.length % 2 == 1 ? a.filter((ele) => ele * 2 !== target) : a;
+  return last.map((ele: number) => {
+    return nums.indexOf(ele);
+  });
+}
---
+++ easy_practice/task-3.ts
@@ -0,0 +1,7 @@
+// 9. Palindrome Number
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+function isPalindrome(x: number): boolean {
+  return x.toString().split("").reverse().join("") == x.toString();
+}
+console.log(isPalindrome(121));
