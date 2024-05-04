12589782a20a9fefcd59e3e0617667eedee6edc2
shon <turayevshahzodbek@gmail.com>
2024-05-04 23:48:43 +0900

    feat: practice 2 medium is solved

---
+++ medium_practice/task-2.ts
@@ -0,0 +1,20 @@
+let numRescueBoats = function (people, limit) {
+  let count = 0,
+    i = 0,
+    j = people.length - 1;
+  people.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(people, "people-after sort");
+  while (i <= j) {
+    if (people[i] + people[j] <= limit) {
+      count++;
+      i++;
+      j--;
+    } else {
+      count++;
+      j--;
+    }
+  }
+  return count;
+};
