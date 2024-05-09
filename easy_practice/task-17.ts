c97f6e58a53563118c37661953cd6de796c2a697
shon <turayevshahzodbek@gmail.com>
2024-05-09 21:57:22 +0900

    feat: 3075. Maximize Happiness of Selected Children

--- easy_practice/task-17.ts
+++ easy_practice/task-17.ts
@@ -1,48 +1,52 @@
-function isValid(word) {
-  let uppercaseArr = [];
-  let lowercaseArr = [];
-  let numberArr = [];
-  let vowelArr = [];
-  let consonantArr = [];
-  const vowelLetters = ["a", "e", "i", "o", "u"];
-  function isValid(word: string): boolean {
-    let uppercaseArr: string[] = [];
-    let lowercaseArr: string[] = [];
-    let numberArr: string[] = [];
-    let vowelArr: string[] = [];
-    let consonantArr: string[] = [];
-    const vowelLetters: string[] = ["a", "e", "i", "o", "u"];
-    for (let i = 0; i < word.length; i++) {
-      if (Number(word[i]) >= 0) {
-        numberArr.push(word[i]);
-      }
-      if (word[i].toUpperCase() == word[i] && !(Number(word[i]) >= 0)) {
-        uppercaseArr.push(word[i]);
-        if (vowelLetters.includes(word[i].toLowerCase())) {
-          vowelArr.push(word[i]);
-        } else {
-          consonantArr.push(word[i]);
-        }
-      }
-      if (word[i].toLowerCase() == word[i] && !(Number(word[i]) >= 0)) {
-        lowercaseArr.push(word[i]);
-        if (vowelLetters.includes(word[i].toLowerCase())) {
-          vowelArr.push(word[i]);
-        } else {
-          consonantArr.push(word[i]);
-        }
-      }
-    }
+// function isValid(word) {
+//   let uppercaseArr = [];
+//   let lowercaseArr = [];
+//   let numberArr = [];
+//   let vowelArr = [];
+//   let consonantArr = [];
+//   const vowelLetters = ["a", "e", "i", "o", "u"];
+//   function isValid(word: string): boolean {
+//     let uppercaseArr: string[] = [];
+//     let lowercaseArr: string[] = [];
+//     let numberArr: string[] = [];
+//     let vowelArr: string[] = [];
+//     let consonantArr: string[] = [];
+//     const vowelLetters: string[] = ["a", "e", "i", "o", "u"];
+//     for (let i = 0; i < word.length; i++) {
+//       if (Number(word[i]) >= 0) {
+//         numberArr.push(word[i]);
+//       }
+//       if (word[i].toUpperCase() == word[i] && !(Number(word[i]) >= 0)) {
+//         uppercaseArr.push(word[i]);
+//         if (vowelLetters.includes(word[i].toLowerCase())) {
+//           vowelArr.push(word[i]);
+//         } else {
+//           consonantArr.push(word[i]);
+//         }
+//       }
+//       if (word[i].toLowerCase() == word[i] && !(Number(word[i]) >= 0)) {
+//         lowercaseArr.push(word[i]);
+//         if (vowelLetters.includes(word[i].toLowerCase())) {
+//           vowelArr.push(word[i]);
+//         } else {
+//           consonantArr.push(word[i]);
+//         }
+//       }
+//     }
 
-    return (
-      word.length > 2 &&
-      (uppercaseArr.length > 0 || lowercaseArr.length > 0) &&
-      vowelArr.length > 0 &&
-      consonantArr.length > 0 &&
-      uppercaseArr.length + lowercaseArr.length + numberArr.length ==
-        word.length
-    );
-  }
-}
+//     return (
+//       word.length > 2 &&
+//       (uppercaseArr.length > 0 || lowercaseArr.length > 0) &&
+//       vowelArr.length > 0 &&
+//       consonantArr.length > 0 &&
+//       uppercaseArr.length + lowercaseArr.length + numberArr.length ==
+//         word.length
+//     );
+//   }
+// }
 
-console.log(isValid("y0Ap"));
+// console.log(isValid("y0Ap"));
+const a = [1, 2, 3, 4];
+const b = a;
+a.push(5);
+console.log(b);
