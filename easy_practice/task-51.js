55c8a7ce0ec41504804f8954943a7e1c653ab774
shon <turayevshahzodbek@gmail.com>
2024-08-14 23:52:04 +0900

    easy: 1507. Reformat Date

--- easy_practice/task-51.js
+++ easy_practice/task-51.js
@@ -20,10 +20,9 @@ function reformatDate(date) {
     .filter((ele) => Number.isInteger(Number(ele)))
     .join("");
   const currentMonth = month.indexOf(date.split(" ")[1]) + 1;
-  //   return `${date.split(" ")[2]}-${
-  //     currentMonth > 9 ? currentMonth : `0${currentMonth}`
-  //   }-${dayDate > 9 ? dayDate : `0${dayDate}`}`;
-  return dayDate;
+  return `${date.split(" ")[2]}-${
+    currentMonth > 9 ? currentMonth : `0${currentMonth}`
+  }-${dayDate > 9 ? dayDate : `0${dayDate}`}`;
 }
 
 console.log(reformatDate("6th Jun 1933"));
