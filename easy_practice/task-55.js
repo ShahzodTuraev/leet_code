8afbe721c6c875a6f090fd94bcea160f3a2f4be7
shon <turayevshahzodbek@gmail.com>
2024-08-22 21:37:25 +0900

    easy: 662. Check If Two String Arrays are Equivalent

---
+++ easy_practice/task-55.js
@@ -0,0 +1,6 @@
+// 1662. Check If Two String Arrays are Equivalent
+function arrayStringsAreEqual(word1, word2) {
+  return word1.join("") == word2.join("");
+}
+
+console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"]));
