2eca4445446f4c3067272e3401581f51828e6079
shon <turayevshahzodbek@gmail.com>
2025-01-26 17:39:27 +0900

    task-77: 171. Excel Sheet Column Number (easy)

---
+++ easy_practice/task-77.js
@@ -0,0 +1,42 @@
+// 171. Excel Sheet Column Number
+function titleToNumber(columnTitle) {
+  const letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let arr = [];
+  let number = 0;
+  for (let i = 0; i < columnTitle.length; i++) {
+    arr.push(letters.indexOf(columnTitle[i]) + 1);
+    number +=
+      (letters.indexOf(columnTitle[i]) + 1) *
+      Math.pow(26, columnTitle.length - i - 1);
+  }
+  return number;
+}
+
+console.log(titleToNumber("A"));
