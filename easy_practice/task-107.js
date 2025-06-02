86ab8bf4d0f5bfec54c5ebb81037bfb029fdb8fc
shon <turayevshahzodbek@gmail.com>
2025-06-03 00:03:48 +0900

    task-107: 2032. Two Out of Three (easy)

--- easy_practice/task-107.js
+++ easy_practice/task-107.js
@@ -1,16 +1,25 @@
+// 2032. Two Out of Three
 function twoOutOfThree(nums1, nums2, nums3) {
+  const innerArrs = [nums1, nums2, nums3];
   const comulArr = nums1.concat(nums2).concat(nums3);
   const distArr = new Set(comulArr);
   const uniqueEles = [...distArr];
+
   let distList = [];
   let freq = 0;
   for (const ele of uniqueEles) {
-    if (nums1.includes(ele)) {
-      freq++;
+    for (const arr of innerArrs) {
+      if (arr.includes(ele)) {
+        freq++;
+      }
     }
+    if (freq >= 2) {
+      distList.push(ele);
+    }
+    freq = 0;
   }
-  // for(let )
-  return comulArr;
+
+  return distList;
 }
 
-console.log(twoOutOfThree([3, 1], [2, 3], [1, 2]));
+console.log(twoOutOfThree([3, 1], [2, 3], [1]));
